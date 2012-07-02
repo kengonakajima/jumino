068d723a137d577e818a415783f3fca34852333f
Kengo Nakajima Deploy User <ken.go.nakajima@gmail.com>
2012-07-03 07:03:56 +0900

    min, max, rect, intersect

--- jumino.js
+++ jumino.js
@@ -27,4 +27,34 @@ p = function(a,b,c,d,e,f,g,h,i,j,k){
 }
 print = p;
 
+rect = function(minx,miny,maxx,maxy) { // name from MOAI
+    return { minx:minx, miny:miny, maxx:maxx, maxy:maxy}    
+}
 
+hit = function(rect1,rect2) {
+    return ( rect1.minx <= rect2.maxx && rect1.miny <= rect2.maxy
+             && rect1.maxx >= rect2.minx && rect1.maxy >= rect2.miny );
+}
+max = function(a,b){ return (a>=b) ? a : b; }
+min = function(a,b){ return (a<=b) ? a : b; }
+
+intersect = function(r1,r2) {
+    if(!hit(r1,r2))return null;
+    var iminx,imaxx;
+    if(r1.minx<=r2.minx){
+        iminx=r2.minx;
+        imaxx=r1.maxx;
+    } else {
+        iminx=r1.minx;
+        imaxx=r2.maxx;
+    }
+    var iminy, imaxy;
+    if(r1.miny<=r2.miny){
+        iminy=r2.miny;
+        imaxy=r1.maxy;
+    } else {
+        iminy=r1.miny;
+        imaxy=r2.maxy;
+    }
+    return rect(iminx,iminy,imaxx,imaxy);
+}
--- test.js
+++ test.js
@@ -17,4 +17,32 @@ assert(tostring(999.99)=="999.99");
 p(1,2,"3",{a:1,b:2});
 p("done");
 
+var r = rect(1,1,3,3);
+assert(r.minx==1);
+assert(r.miny==1);
+assert(r.maxx==3);
+assert(r.maxy==3);
+var r2 = rect(2,2,4,4);
+var r3 = rect(5,5,6,6);
+assert( hit(r,r2) );
+assert( !hit(r,r3) );
+
+var ir1 = intersect(r,r2);
+p(ir1);
+assert( ir1.minx==2 );
+assert( ir1.miny==2 );
+assert( ir1.maxy==3 );
+assert( ir1.maxy==3 );
+var ir2 = intersect(r2,r);
+p(ir2);
+assert( ir2.minx==2 );
+assert( ir2.miny==2 );
+assert( ir2.maxy==3 );
+assert( ir2.maxy==3 );
+
+        
+assert( min(1,2)==1);
+assert( min(2,1)==1);
+assert( max(2,1)==2);
+assert( max(1,2)==2);
 
